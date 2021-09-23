b0382ab35c1ebb82bb340e60520da5ed6e4b5aea
dengxuhui <419270388@qq.com>
2021-09-23 15:11:01 +0800

    ui基类

---
+++ UnityBlogCode/Assets/Script/UI/UIPrimitiveBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UnityEngine.UI.Extensions
+{
+    // /// <summary>
+    // /// ui扩展基类
+    // /// </summary>
+    // public class UIPrimitiveBase : MaskableGraphic, ILayoutElement, ICanvasRaycastFilter
+    // {
+    //     [SerializeField] private Sprite m_Sprite;
+    //     public Sprite sprite
+    //     {
+    //         get { return m_Sprite; }
+    //         // set{}
+    //     }
+    //
+    //     protected override void OnEnable()
+    //     {
+    //         base.OnEnable();
+    //         SetAllDirty();
+    //     }
+    // }
+}
---
+++ UnityBlogCode/Assets/Script/Utilities/SetPropertyUtility.cs
@@ -0,0 +1,8 @@
+﻿namespace UnityEngine.UI.Extensions
+{
+    //属性设置
+    internal static class SetPropertyUtility
+    {
+        // public static bool SetClass<T>(ref T )
+    }
+}
