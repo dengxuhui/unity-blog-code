34280f4f4ca4d799fd4f024e2e06cea500b44b2a
dengxuhui <419270388@qq.com>
2021-09-23 15:53:30 +0800

    utility

--- UnityBlogCode/Assets/Script/Utilities/SetPropertyUtility.cs
+++ UnityBlogCode/Assets/Script/Utilities/SetPropertyUtility.cs
@@ -3,6 +3,23 @@
     //属性设置
     internal static class SetPropertyUtility
     {
-        // public static bool SetClass<T>(ref T )
+        
+        /// <summary>
+        /// 设置类
+        /// </summary>
+        /// <param name="currentValue"></param>
+        /// <param name="newValue"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static bool SetClass<T>(ref T currentValue, T newValue) where T : class
+        {
+            if ((currentValue == null && newValue == null) || (currentValue != null && currentValue.Equals(newValue)))
+            {
+                return false;
+            }
+
+            currentValue = newValue;
+            return true;
+        }
     }
 }
