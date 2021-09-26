c2b7abd69e298a3d0c618e6f1b7381419f9736a9
dengxuhui <419270388@qq.com>
2021-09-26 17:55:34 +0800

    ui画线工具

--- UnityBlogCode/Assets/Script/UI/UILineRenderer.cs
+++ UnityBlogCode/Assets/Script/UI/UILineRenderer.cs
@@ -1,8 +1,17 @@
 ﻿namespace UnityEngine.UI.Extensions
 {
+    /// <summary>
+    /// canvas上画线
+    /// </summary>
     [RequireComponent(typeof(RectTransform))]
     public class UILineRenderer : UIPrimitiveBase
     {
-        
+        private enum SegmentType
+        {
+            Start,
+            Middle,
+            End,
+            Full,
+        }
     }
 }
