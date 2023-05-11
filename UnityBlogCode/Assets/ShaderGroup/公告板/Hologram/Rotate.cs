59add26f4cd509e1453429bc270856e99b45414c
dengxuhui <419270388@qq.com>
2023-05-11 15:33:24 +0800

    全息影像动画

---
+++ UnityBlogCode/Assets/ShaderGroup/公告板/Hologram/Rotate.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float Speed = 30f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, Speed * Time.deltaTime);
+    }
+}
